ee547b3f6aca12ad571410a76d51d23e5d48a810
Abir Ashfaq <abir.ashfaq16@gmail.com>
2025-10-08 20:20:23 +0600

    Add Responsive Banner Section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import "./Banner.css"
 const Banner = () => {
     return (
         <div className='text-center mt-[60px]'>
-            <h1 className='text-[60px] font-bold '>We Build<br /><span className='text-[#9F62F2]' >Productive</span> Apps</h1>
-            <p className='text-[#627382] text-[17px]' >At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+            <h1 className='text-[60px] banner-title font-bold '>We Build<br /><span className='text-[#9F62F2]' >Productive</span> Apps</h1>
+            <p className='text-[#627382] banner-info text-[17px]' >At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br className='hide' /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
             <div className='mt-4' >
                 <button className='btn mr-3'><i class="fa-brands fa-google-play"></i>Google Play</button>
                 <button className='btn' ><i class="fa-brands fa-app-store-ios"></i>App Store</button>
