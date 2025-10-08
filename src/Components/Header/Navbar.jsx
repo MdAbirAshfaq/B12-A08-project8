57fed2c032d23f24351eae2e9da70c8acf1efde2
Abir Ashfaq <abir.ashfaq16@gmail.com>
2025-10-08 19:07:20 +0600

    Add Responsive Navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import "./Navbar.css"
 const Navbar = () => {
     return (
         <div>
-            <h1>ami nav</h1>
+            <div className="navbar bg-base-100 w-11/12 m-auto ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li className='mx-4' >Home</li>
+        <li className='mx-4' >Apps</li>
+        <li className='mx-2' >Installation</li>
+      </ul>
+    </div>
+    <a className="btn hover:bg-white font-bold btn-ghost text-[#9F62F2] text-xl"> <img className='w-[45px] h-[45px]' src="/src/assets/logo.png" alt="logo" />HERO.IO</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li className='mx-4' >Home</li>
+      <li className='mx-4' >Apps</li>
+      <li className='mx-2' >Installation</li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn text-white bg-linear-to-r from-[#632EE3] to-[#9F62F2] "><i class="fa-brands fa-github"></i>Contribute</a>
+  </div>
+</div>
         </div>
     );
 };
